cd2d2e03544f63a34046a84e2d97d449625bfa94
lixiaonan <472379153@qq.com>
2022-07-30 23:24:13 +0800

    覆写一个类对象的方法 生成的类是 继承这个类的匿名类对

---
+++ src/main/java/Jvm/HandleTest.java
@@ -0,0 +1,10 @@
+package Jvm;
+
+/**
+ * @author：李晓楠 时间：2022/7/29 15:11
+ */
+public class HandleTest {
+
+    public void handleMessage() {
+    }
+}
---
+++ src/main/java/Jvm/MainActivityTest.java
@@ -0,0 +1,26 @@
+package Jvm;
+
+/**
+ * @author：李晓楠 时间：2022/7/29 15:12
+ */
+public class MainActivityTest {
+
+    private static HandleTest handleTest = new HandleTest();
+
+    /**
+     * MainActivityTest$1 类对象为这个
+     * 是一个继承了 HandleTest 的一个新的匿名类 类名为这个 MainActivityTest$1
+     * 如果使用了这种匿名类要主要 处理内存泄漏的问题
+     */
+    private static HandleTest handleTest1 = new HandleTest(){
+        @Override
+        public void handleMessage() {
+            super.handleMessage();
+        }
+    };
+
+    public static void main(String[] args) {
+        System.out.println("=="+handleTest.getClass());
+        System.out.println("=="+handleTest1.getClass());
+    }
+}
