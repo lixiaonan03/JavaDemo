7317bbc25baf77bfcf0a3fa064034657ef256d95
lixiaonan <472379153@qq.com>
2022-11-04 10:02:10 +0800

    kotlin flow的一些测试

--- src/main/kotlin/kotlintest/flowTest.kt
+++ src/main/kotlin/kotlintest/flowTest.kt
@@ -14,6 +14,16 @@ object flowTest {
 
     @JvmStatic
     fun main(args: Array<String>) {
+        println("之前的的==${Thread.currentThread().name}")
+
+        val ss =GlobalScope.launch{
+            println("开始前==${Thread.currentThread().name}")
+            val dd = makeLoinRequest("测试的12333")
+            println("开始后==${Thread.currentThread().name}")
+        }
+
+        println("之后的==${Thread.currentThread().name}")
+        Thread.sleep(10000)
 //        GlobalScope.async {
 //            flowTest()
 //        }
@@ -26,6 +36,11 @@ object flowTest {
 //
 //        }
 
+        // Job and Dispatcher are combined into a CoroutineContext which
+        // will be discussed shortly
+        val scope = CoroutineScope(Job() + Dispatchers.Main).launch {
+
+        }
     }
 
 //    private suspend fu = SparseArray<Long>()  n flowTest(){
@@ -38,5 +53,18 @@ object flowTest {
 //            println(it)
 //        }
 //    }
+
+    suspend fun makeLoinRequest(jsonBody: String): String {
+        delay(2000)
+        println("协=$jsonBody===${Thread.currentThread().name}")
+        return "的"
+
+//        return withContext(Dispatchers.IO){
+//            println("协程的==${Thread.currentThread().name}")
+//            delay(2000)
+//            return@withContext "的"
+//        }
+    }
+
 }
 
