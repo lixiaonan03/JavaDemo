937749da821b95258b94177a15564655c5bbaa38
lixiaonan <472379153@qq.com>
2022-11-04 09:15:33 +0800

    String对象内存地址对比的说明

--- src/main/java/Jvm/MainActivityTest.java
+++ src/main/java/Jvm/MainActivityTest.java
@@ -23,10 +23,20 @@ public class MainActivityTest {
         System.out.println("=="+handleTest.getClass());
         System.out.println("=="+handleTest1.getClass());
 
+
+        // https://blog.csdn.net/qwe123147369/article/details/113104651
         final String s1 = "aa";
         final String s2 = "bb";
         String str1 = s1 + s2;
         String str2 = "aabb";
-        System.out.println(str1.equals(str2));
+        //这个返回的是true final 修饰的编译器会优化  str1 = "aabb"
+        System.out.println(str1 == str2);
+
+        String s11 = "aa";
+        String s21 = "bb";
+        String str11 = s11 + s21;
+        String str21 = "aabb";
+        //str11 生成的时候会用 StringBuilder 生成对象的  对比内存地址的话 str11 和 str21 是不一样的
+        System.out.println(str11 == str21);
     }
 }
