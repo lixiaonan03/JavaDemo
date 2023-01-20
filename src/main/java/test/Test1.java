9f18aa3ec41815c958605019b1c9d00775cf447a
lixiaonan <472379153@qq.com>
2023-01-20 10:42:39 +0800

    String 测试类

---
+++ src/main/java/test/Test1.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * @author：李晓楠 时间：2022/8/17 15:54
+ */
+public class Test1 {
+
+
+    public static void main(String[] args) throws Exception {
+        String aa = "asdf";
+        System.out.println(aa.substring(0,4));
+    }
+
+}
+
---
+++ src/main/kotlin/kotlintest/str/StringTest.kt
@@ -0,0 +1,23 @@
+package kotlintest.str
+
+import kotlintest.ListTest
+
+/**
+ * @author：李晓楠
+ * 时间：2023/1/18 14:59
+ */
+object StringTest {
+
+    @JvmStatic
+    fun main(vararg args: String) {
+        val numStr = "02631111111111111"
+        val numSub= numStr.substring(3)
+        println("截取的字符串==$numSub")
+        val count = numSub.count { it == numSub[1] }
+        println("count==$count")
+        val yes = count +3 == numStr.length
+        println("numStr 是否重复的==$yes==")
+
+        println( numStr.substring(3).count { it == numStr[3] } + 3 == numStr.length)
+    }
+}
